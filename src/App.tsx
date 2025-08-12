66259e19dfde001fee7b3f6fa0275cac959fa6ed
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-08-12 21:23:05 +0900

    chore: PR 테스트를 위한 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="w-screen h-screen bg-white">
-      <h1 className="text-black text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-black text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
