a715bf5acd02e4144886b4ac7fd58e24aa203ae3
xeoxxn <yiseojun1@kookmin.ac.kr>
2025-08-13 22:06:32 +0900

    Rollback: Fix mistake in login feature

--- src/store.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-import type { Store } from './types';
-
-export const useStore = create<Store>((set) => ({
-  data: '',
-  setData: (newData) => set({ data: newData }),
-}));
--- src/types.ts
+++
@@ -1,4 +0,0 @@
-export interface Store {
-  data: string;
-  setData: (newData: string) => void;
-}
