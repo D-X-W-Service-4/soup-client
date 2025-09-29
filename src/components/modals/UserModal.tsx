308484c670ebf194a5b4024fd894f3d22018969b
이서준 <104981505+xeoxxn@users.noreply.github.com>
2025-09-29 12:30:36 +0900

    fix: tailwind 관련 값 통일화

--- src/components/modals/UserModal.tsx
+++ src/components/modals/UserModal.tsx
@@ -15,6 +15,7 @@ const UserModal = () => {
   const [semester, setSemester] = useState('');
   const [studyTime, setStudyTime] = useState('');
 
+  /* 닉네임 변경 함수 - 에러(영어/한글/숫자만) */
   const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setNickname(value);
@@ -22,12 +23,14 @@ const UserModal = () => {
     setError(value && !regex.test(value) ? true : false);
   };
 
+  /* 공부 시간 관련 함수 (숫자만 추출하도록) */
   const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     const onlyNumber = value.replace(/\D/g, ''); // 숫자만 추출
     setStudyTime(onlyNumber);
   };
 
+  /* 저장되면 초기화 */
   const handleSave = () => {
     setNickname('');
     setError(false);
@@ -47,44 +50,46 @@ const UserModal = () => {
         onClick={toggleUserModal}
       ></div>
 
-      <div className="relative z-10 inline-flex h-96 w-[794px] flex-col items-start justify-start gap-5 rounded-[20px] bg-white px-10 py-7 shadow-[0px_2px_10px_0px_rgba(0,0,0,0.10)]">
+      <div className="relative z-10 inline-flex h-96 w-194.5 flex-col items-start justify-start gap-5 rounded-2xl bg-white px-10 py-7 shadow-[0px_2px_10px_0px_rgba(0,0,0,0.10)]">
         <div className="flex w-32 flex-col items-start justify-start gap-2.5">
-          <div className="self-stretch font-['Pretendard_Variable'] text-xl leading-7 font-medium text-black">
+          <div className="self-stretch text-xl leading-7 font-medium text-black">
             내 정보 수정
           </div>
-          <div className="self-stretch font-['Pretendard_Variable'] text-sm leading-tight font-normal text-neutral-600">
+          <div className="self-stretch text-sm leading-tight font-normal text-neutral-600">
             내 정보를 수정합니다.
           </div>
         </div>
 
+        {/* 닉네임 수정 */}
         <div className="flex flex-col items-start justify-start gap-5 self-stretch overflow-y-auto">
           <div className="flex flex-col items-start justify-start gap-2 self-stretch">
-            <div className="font-['Pretendard_Variable'] text-base leading-normal font-medium text-black">
+            <div className="text-base leading-normal font-medium text-black">
               닉네임 수정
             </div>
-            <div className="inline-flex items-center self-stretch rounded-[10px] bg-neutral-100 px-3 py-3.5">
+            <div className="inline-flex items-center self-stretch rounded-lg bg-neutral-100 px-3 py-3.5">
               <input
-                className="h-[1em] w-full font-['Pretendard_Variable'] text-xs font-normal text-neutral-600 placeholder:text-zinc-500 focus:outline-none"
-                placeholder="닉네임을 입력하세요"
+                className="h-[1em] w-full text-xs font-normal text-neutral-600 placeholder:text-zinc-500 focus:outline-none"
+                placeholder="사용하실 닉네임을 입력해주세요!"
                 value={nickname}
                 onChange={handleNicknameChange}
               />
             </div>
             {error && (
-              <div className="font-['Pretendard_Variable'] text-xs leading-none font-normal text-red-500">
+              <div className="text-xs leading-none font-normal text-red-500">
                 올바르지 않은 닉네임입니다.
               </div>
             )}
           </div>
 
+          {/* 학년 입력 */}
           <div className="flex flex-col items-start justify-center gap-2 self-stretch">
-            <div className="font-['Pretendard_Variable'] text-base leading-normal font-medium text-black">
+            <div className="text-base leading-normal font-medium text-black">
               현재 학년 입력
             </div>
             <div className="w-full">
               {isGradeOpen ? (
-                <div className="mt-1 flex w-full flex-col overflow-hidden rounded-[10px] bg-neutral-100">
-                  {['1학년', '2학년', '3학년', '4학년'].map((item) => (
+                <div className="mt-1 flex w-full flex-col overflow-hidden rounded-lg bg-neutral-100">
+                  {['1학년', '2학년', '3학년'].map((item) => (
                     <div
                       key={item}
                       className="cursor-pointer px-3 py-3.5 text-sm hover:bg-neutral-200"
@@ -99,11 +104,11 @@ const UserModal = () => {
                 </div>
               ) : (
                 <div
-                  className="inline-flex w-full cursor-pointer items-center justify-between rounded-[10px] bg-neutral-100 px-3 py-3.5"
+                  className="inline-flex w-full cursor-pointer items-center justify-between rounded-lg bg-neutral-100 px-3 py-3.5"
                   onClick={() => setIsGradeOpen(!isGradeOpen)}
                 >
-                  <div className="font-['Pretendard_Variable'] text-xs leading-none font-normal text-zinc-500">
-                    {grade === '' ? '현재 학년을 선택해주세요.' : grade}
+                  <div className="text-xs leading-none font-normal text-zinc-500">
+                    {grade === '' ? '현재 학년을 선택해주세요!' : grade}
                   </div>
                   <ChevronDownIcon className="h-4 w-4 text-neutral-600" />
                 </div>
@@ -111,13 +116,14 @@ const UserModal = () => {
             </div>
           </div>
 
+          {/* 학기 입력 */}
           <div className="flex flex-col items-start justify-center gap-2 self-stretch">
-            <div className="font-['Pretendard_Variable'] text-base leading-normal font-medium text-black">
+            <div className="text-base leading-normal font-medium text-black">
               현재 학기 입력
             </div>
             <div className="w-full">
               {isSemesterOpen ? (
-                <div className="mt-1 flex w-full flex-col overflow-hidden rounded-[10px] bg-neutral-100">
+                <div className="mt-1 flex w-full flex-col overflow-hidden rounded-lg bg-neutral-100">
                   {['1학기', '2학기'].map((item) => (
                     <div
                       key={item}
@@ -133,11 +139,11 @@ const UserModal = () => {
                 </div>
               ) : (
                 <div
-                  className="inline-flex w-full cursor-pointer items-center justify-between rounded-[10px] bg-neutral-100 px-3 py-3.5"
+                  className="inline-flex w-full cursor-pointer items-center justify-between rounded-lg bg-neutral-100 px-3 py-3.5"
                   onClick={() => setIsSemesterOpen(!isSemesterOpen)}
                 >
-                  <div className="font-['Pretendard_Variable'] text-xs leading-none font-normal text-zinc-500">
-                    {semester === '' ? '현재 학기를 선택해주세요.' : semester}
+                  <div className="text-xs leading-none font-normal text-zinc-500">
+                    {semester === '' ? '현재 학기를 선택해주세요!' : semester}
                   </div>
                   <ChevronDownIcon className="h-4 w-4 text-neutral-600" />
                 </div>
@@ -145,14 +151,15 @@ const UserModal = () => {
             </div>
           </div>
 
+          {/* 공부 시간 입력 */}
           <div className="flex flex-col items-start justify-start gap-2 self-stretch">
-            <div className="font-['Pretendard_Variable'] text-base leading-normal font-medium text-black">
+            <div className="text-base leading-normal font-medium text-black">
               공부시간 입력
             </div>
-            <div className="inline-flex items-center self-stretch rounded-[10px] bg-neutral-100 px-3 py-3.5">
+            <div className="inline-flex items-center self-stretch rounded-lg bg-neutral-100 px-3 py-3.5">
               <input
-                className="h-[1em] w-full font-['Pretendard_Variable'] text-xs font-normal text-neutral-600 placeholder:text-zinc-500 focus:outline-none"
-                placeholder="공부 가능한 시간을 입력하세요"
+                className="h-[1em] w-full text-xs font-normal text-neutral-600 placeholder:text-zinc-500 focus:outline-none"
+                placeholder="하루에 공부 가능한 시간을 입력해주세요!"
                 value={studyTime}
                 onChange={handleTimeChange}
                 onBlur={(e) => {
@@ -170,7 +177,7 @@ const UserModal = () => {
           onClick={handleSave}
         >
           <div className="inline-flex cursor-pointer items-center justify-center gap-2.5 self-stretch rounded-lg bg-red-400 px-5 py-3">
-            <div className="font-['Pretendard_Variable'] text-base leading-normal font-medium text-white">
+            <div className="text-base leading-normal font-medium text-white">
               내 정보 수정
             </div>
           </div>
