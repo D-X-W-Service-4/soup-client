39f52ef929cb6f57a80e0be877f11fa4f59a5425
chael-in <cofls3511@gmail.com>
2025-11-04 11:21:26 +0900

    feat: QuestionCard 컴포넌트 구현

--- src/pages/test/result/components/QuestionCard.tsx
+++ src/pages/test/result/components/QuestionCard.tsx
@@ -26,29 +26,35 @@ export default function QuestionCard({
   return (
     <div className="flex items-center justify-between rounded-md px-6 py-5 outline outline-1 outline-offset-[-1px] outline-neutral-100">
       <div className="flex items-center justify-start gap-5">
-        <Icon icon={statusIcon} className={`h-4 w-4 ${statusColor}`} />
+        <Icon icon={statusIcon} className={`h-5 w-5 ${statusColor}`} />
         <div className="flex flex-col">
-          <span className="text-base font-semibold">{question}</span>
+          <div className="flex gap-5">
+            <span className="text-base font-semibold">{question}</span>
+            {isStarred && (
+              <Icon
+                icon="material-symbols:star-rounded"
+                className="h-6 w-6 text-yellow-200"
+              />
+            )}
+          </div>
           <span className="text-xs font-normal text-neutral-500">
             {tryCount}번 시도
           </span>
         </div>
-        {isStarred && (
-          <Icon
-            icon="material-symbols:star-rounded"
-            className="h-4 w-5 border-yellow-200 bg-yellow-200"
-          />
-        )}
       </div>
-      <div className="flex gap-[14px]">
+      <div className="flex items-center gap-[14px]">
         <DifficultyBadge variant={difficulty} />
         <span className="text-sm font-medium text-neutral-600">
           {createdAt}
         </span>
         <button
+          type="button"
           onClick={() => {}}
-          className="text-sm font-medium text-primary underline"
+          className="flex items-center gap-2 rounded-lg bg-neutral-300 px-3 py-2 text-xs font-medium text-white
+                 hover:bg-secondary active:bg-secondary-bg"
+          aria-label="다시 풀기"
         >
+          <Icon icon="mingcute:play-fill" className="h-3.5 w-3.5" />
           다시 풀기
         </button>
       </div>
