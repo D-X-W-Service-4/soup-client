7c07d4c1c1a53779ca575fa4278784dc95e69bbd
chael-in <cofls3511@gmail.com>
2025-11-14 05:00:36 +0900

    style: layout 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ import UserInfoModal from './UserInfoModal'; // 모달만 임포트
 export default function Layout() {
   return (
     <div className="flex h-dvh w-full items-center justify-center bg-gray-100">
-      <main className="h-[834px] w-[1194px] overflow-y-auto rounded-lg bg-white shadow-xl">
+      <main className="h-full w-full overflow-y-auto bg-white">
         <Outlet />
       </main>
 
